190ad3529bd2f61894034efb8c347659361b47e3
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-02 19:04:44 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "27fe0cbf-1710-4e06-9d8f-367ab1164fb1",
+    "home_feed": [
+        {
+            "id": "b6c5a533-4479-4ee9-b6e1-4faf3b2b33d3",
+            "artist_name": "life",
+            "home_carousel": [
+                {
+                    "id": "e8504af0-081f-4412-8c3c-bb372a96060e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "e3475ad4-c0e8-40c6-b90b-6f97b51d1087",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/65.png"
+                },
+                {
+                    "id": "72252882-ce23-4281-8ce0-7322cef77e1b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/243.png"
+                },
+                {
+                    "id": "90b3c1f3-3c98-4414-b3fa-824eb811f14f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png"
+                },
+                {
+                    "id": "27225bb2-e33b-445e-b54f-1058e43e997d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                }
+            ],
+            "art_caption": "true",
+            "art_description": "Travel industry air down there some onto beat. Wonder control son memory record."
+        },
+        {
+            "id": "6377f40c-0855-4022-a172-f6b86614f514",
+            "artist_name": "necessary",
+            "home_carousel": [
+                {
+                    "id": "73e8ba7a-6826-4f40-a967-7ab5ff24076e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/165.png"
+                },
+                {
+                    "id": "098a147d-89b8-462b-a8db-b5e41e42498a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/115.png"
+                },
+                {
+                    "id": "6ccdd864-0847-41be-a1d3-282cc8f22ff9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "151e8c00-75f6-4de2-9b5c-3dfbd77bf7d9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png"
+                },
+                {
+                    "id": "4b24e8d1-56dd-4244-ad8d-fe10a0555b8a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/70.png"
+                }
+            ],
+            "art_caption": "everyone",
+            "art_description": "Particularly painting threat us speech adult. Hospital only country customer human. Low outside know resource head method to pull."
+        },
+        {
+            "id": "2f51ff82-7b74-4fd7-a438-93766f91d2b7",
+            "artist_name": "about",
+            "home_carousel": [
+                {
+                    "id": "a1bfc636-98f1-410f-8b14-44942e55df4e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/261.png"
+                },
+                {
+                    "id": "08e37628-1524-4b2d-a685-01d7e4af082c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+                },
+                {
+                    "id": "e5b4f72f-b076-4b7c-8ffb-0867e61f23b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/24.png"
+                },
+                {
+                    "id": "977dc29b-5ea3-4658-b881-c2c7cfcab410",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/136.png"
+                },
+                {
+                    "id": "bd315bbc-e843-4560-b648-d05a2fd38b25",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/274.png"
+                }
+            ],
+            "art_caption": "behind",
+            "art_description": "Condition government recognize produce to stop far."
+        },
+        {
+            "id": "fff32712-2b97-454d-94b9-d5e0f5619b15",
+            "artist_name": "community",
+            "home_carousel": [
+                {
+                    "id": "ac04c674-5a9e-45e2-88f8-d39ebe92ab71",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/221.png"
+                },
+                {
+                    "id": "0c10966c-b4c7-48ad-bfad-f007039c55ba",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/57.png"
+                },
+                {
+                    "id": "72dd3ee9-789c-465a-8d59-21120b522c22",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/278.png"
+                },
+                {
+                    "id": "78c35d20-cd3e-4bd3-b605-c652f6274821",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/126.png"
+                },
+                {
+                    "id": "9a587e23-bcaa-4454-a8fe-7e6ba96030c2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/367.png"
+                }
+            ],
+            "art_caption": "model",
+            "art_description": "Difficult rise fire produce vote modern. Common structure member direction financial send participant. Around result will charge future force yourself cold."
+        },
+        {
+            "id": "c670d3d7-98b2-460a-8616-dc39028548de",
+            "artist_name": "oil",
+            "home_carousel": [
+                {
+                    "id": "b941b558-354a-47ff-98c3-757d1f9bb3a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png"
+                },
+                {
+                    "id": "c8d88fff-18b1-4bbc-bd8d-433645f99fee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/73.png"
+                },
+                {
+                    "id": "c5f00f35-8348-4e73-b489-01f7fce2c330",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png"
+                },
+                {
+                    "id": "1d2117b1-7d22-4935-8aeb-6668aaf4746d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/275.png"
+                },
+                {
+                    "id": "8bd59b28-7452-49a7-a319-f089e0ec9474",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/30.png"
+                }
+            ],
+            "art_caption": "onto",
+            "art_description": "Lose fund consider affect cold argue nothing. Tonight technology news participant second may develop. Cold act vote occur force."
+        }
+    ]
+}
